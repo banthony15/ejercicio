e47d6423adc9ebce22da357e28365e79b8a44d3b
uchihaslim <>
2018-01-11 10:36:35 -0400

    modificacion en el header

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -124,7 +124,7 @@
               <!-- The user image in the navbar-->
               <img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs">{{ Auth::user()->name }}</span>
+{{--               <span class="hidden-xs">{{ Auth::user()->name }} </span> --}} ->indica el usuario que esta conectado
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
@@ -132,8 +132,8 @@
                 <img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  {{Auth::user()->name}}
-                  <small>CICPC</small>
+                  {{-- {{ Auth::user()->name }}1 --}} ->indica el usuario que esta conectado
+                  <small>Member since Nov. 2012</small>
                 </p>
               </li>
               <!-- Menu Body -->
